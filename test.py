968546cfcbd2a7bb0d480e39f5731bee8254ecea
srt19 <ari.kurniawan2026@students.unila.ac.id>
2023-01-23 12:23:24 +0700

    Added checking existing file in chapter folder

--- test.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--compress", action="store_true",
-                    help="Compress File")
-argL = parser.parse_args()
-
-print(argL.compress)
